b78792bcf9396d9b7589823404daed309fb35254
clarayuki <cyseolar@gmail.com>
2024-07-08 22:58:47 -0300

    🎨 Update indentation

--- day1-a/src/main.rs
+++ day1-a/src/main.rs
@@ -6,17 +6,17 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let result: i32 = reader
-                        .lines()
-                        .into_iter()
-                        .map(|line| {
-                            let line = line.expect("Failed to read line");
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line = line.expect("Failed to read line");
 
-                            let first =  line.bytes().find(u8::is_ascii_digit).unwrap() - b'0';
-                            let last = line.bytes().rfind(u8::is_ascii_digit).unwrap() - b'0';
+            let first =  line.bytes().find(u8::is_ascii_digit).unwrap() - b'0';
+            let last = line.bytes().rfind(u8::is_ascii_digit).unwrap() - b'0';
 
-                            10 * first as i32 + last as i32
-                        })
-                        .sum();
+            10 * first as i32 + last as i32
+        })
+        .sum();
 
     println!("Total calibration value: {}", result);
 
--- day1-b/src/main.rs
+++ day1-b/src/main.rs
@@ -9,43 +9,43 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let result: i32 = reader
-                        .lines()
-                        .into_iter()
-                        .map(|line| {
-                            let line = line.expect("Failed to read line");
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line = line.expect("Failed to read line");
 
-                            let mut bytes = line.as_bytes();
+            let mut bytes = line.as_bytes();
 
-                            let first = 'first: loop {
-                                if bytes[0].is_ascii_digit() {
-                                    break (bytes[0] - b'0') as usize
-                                }
+            let first = 'first: loop {
+                if bytes[0].is_ascii_digit() {
+                    break (bytes[0] - b'0') as usize
+                }
 
-                                for (index, digit) in DIGITS.iter().enumerate() {
-                                    if bytes.starts_with(digit) {
-                                        break 'first index + 1
-                                    }
-                                }
-                                bytes = &bytes[1..];
-                            };
+                for (index, digit) in DIGITS.iter().enumerate() {
+                    if bytes.starts_with(digit) {
+                        break 'first index + 1
+                    }
+                }
+                bytes = &bytes[1..];
+            };
 
-                            let last = 'last: loop {
-                                if bytes[bytes.len() - 1].is_ascii_digit() {
-                                    break (bytes[bytes.len() - 1] - b'0') as usize
-                                }
+            let last = 'last: loop {
+                if bytes[bytes.len() - 1].is_ascii_digit() {
+                    break (bytes[bytes.len() - 1] - b'0') as usize
+                }
 
-                                for (index, digit) in DIGITS.iter().enumerate() {
-                                    if bytes.ends_with(digit) {
-                                        break 'last index + 1
-                                    }
-                                }
-                                bytes = &bytes[..bytes.len() - 1];
-                            };
+                for (index, digit) in DIGITS.iter().enumerate() {
+                    if bytes.ends_with(digit) {
+                        break 'last index + 1
+                    }
+                }
+                bytes = &bytes[..bytes.len() - 1];
+            };
 
-                            10 * first as i32 + last as i32
+            10 * first as i32 + last as i32
 
-                        })
-                        .sum();
+        })
+        .sum();
 
     println!("Total calibration value: {}", result);
 
